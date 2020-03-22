4b4363b7d84243047f01f73089b0c1ad095f8a50
Angelina German <angyricci.art@gmail.com>
2020-03-22 21:06:32 +0300

    feat: add solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,25 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let element = '';
+  let firstLetter = '';
+  let result = '';
+
+  if (!(members instanceof Array)) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] !== 'string') {
+      continue;
+    }
+
+     element = members[i].toString().trim().toUpperCase();
+     firstLetter = element.charAt(0);
+     result = result.concat(firstLetter).split('').sort().join('');
+  }
+
+  if (result === '') {
+    return false;
+  }
+
+  return result;
 };
